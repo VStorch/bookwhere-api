04f66f536baeb38b3f9d5aaaee078720fe375286
Vinícius Storch <viniciusstorch62@gmail.com>
2026-03-18 18:54:50 -0300

    First commit

---
+++ src/main/java/com/viniciusstorch/bookwhere_api/BookwhereApiApplication.java
@@ -0,0 +1,13 @@
+package com.viniciusstorch.bookwhere_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookwhereApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookwhereApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viniciusstorch/bookwhere_api/BookwhereApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viniciusstorch.bookwhere_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookwhereApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
