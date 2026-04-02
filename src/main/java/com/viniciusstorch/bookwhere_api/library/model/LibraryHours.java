81a7e56eeb6587a517f10ca4959809b523fdd0f5
Vinícius Storch <viniciusstorch62@gmail.com>
2026-04-01 21:22:16 -0300

    refactor(library): add lombok constructor to LibraryHours

--- src/main/java/com/viniciusstorch/bookwhere_api/library/model/LibraryHours.java
+++ src/main/java/com/viniciusstorch/bookwhere_api/library/model/LibraryHours.java
@@ -11,13 +11,18 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Entity
 @Table(name = "library_hours")
 @Getter
 @Setter
+@AllArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class LibraryHours {
     
     @Id
