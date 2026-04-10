3cf710fe9c591097c6c80a9262e161f3e94175a4
Vinícius Storch <viniciusstorch62@gmail.com>
2026-04-10 20:32:49 -0300

    feat(book): add book copy entity and book status enum

---
+++ src/main/java/com/viniciusstorch/bookwhere_api/book/model/BookCopy.java
@@ -0,0 +1,21 @@
+package com.viniciusstorch.bookwhere_api.book.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "book_copies")
+public class BookCopy {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private BookStatus status;
+}
---
+++ src/main/java/com/viniciusstorch/bookwhere_api/book/model/BookStatus.java
@@ -0,0 +1,8 @@
+package com.viniciusstorch.bookwhere_api.book.model;
+
+public enum BookStatus {
+    AVAILABLE,
+    BORROWED,
+    RESERVED,
+    LOST
+}
