54195e98b4c43d0ec260a02ee7b4db1dea05c7a7
Vinícius Storch <viniciusstorch62@gmail.com>
2026-04-10 19:17:42 -0300

    feat(book) add author entity

---
+++ src/main/java/com/viniciusstorch/bookwhere_api/book/model/Author.java
@@ -0,0 +1,28 @@
+package com.viniciusstorch.bookwhere_api.book.model;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "authors")
+@Getter
+@Setter
+public class Author {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private LocalDate birthDate;
+
+    private LocalDate deathDate;
+}
